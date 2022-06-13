739243ce3c8cafefa2621497cab74c888ad5c93c
Dinko <dinko.dugec@gmail.com>
2022-06-13 23:19:55 +0200

    define consant

--- index.php
+++ index.php
@@ -1,7 +1,17 @@
 <?php
 
-echo '<pre>';
+//https://stackify.com/display-php-errors/
+// odkomentirati kada želimo vidjeti sva upozorenja i greške
 
-print_r($_SERVER); //$_SERVER is an array containing information such as headers, paths, and script locations. T
+/* ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL); */
 
-echo '<pre>';
+
+
+session_start(); // globalna varijabla na serveru i podaci se pohranjuju dok je session aktivan i sacuvani su na serveru
+
+//echo __DIR__;
+define('BP',__DIR__ . DIRECTORY_SEPARATOR);     //__DIR__ magic constant it returns the directory of the file, technically speaking, the __DIR__ is equivalent to the dirname(__FILE__)
+
+echo BP;
